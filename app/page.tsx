d5ddd224422156875f4d5cd27c24f1d4825160fb
ysm <ysm40595@naver.com>
2023-11-13 23:51:36 +0900

    타이틀 변경

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">NIA 측정 결과 입력 시스템</h1>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">NIA 측정 결과 입력<br/> 시스템</h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">SK텔레콤 수도권 Infra NIA 측정 결과 입력</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a href="/inputform" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">입력하러 가기</a>
